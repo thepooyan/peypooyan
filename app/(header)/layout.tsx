27bb0ceb0159f74c66bfea7d65f0d651d5bcf109
pooyan <thepooyan@gmail.com>
2026-02-12 16:19:12 +0330

    nothing!

--- app/(header)/layout.tsx
+++ app/(header)/layout.tsx
@@ -7,11 +7,11 @@ interface p {
 }
 const layout = ({children}:p) => {
   return (
-    <main className="min-h-screen bg-background text-foreground">
+    <div className="min-h-screen bg-background text-foreground">
       <Header />
       {children}
       <Footer />
-    </main>
+    </div>
   )
 }
 
--- todo.md
+++ todo.md
@@ -1,11 +1,14 @@
-seo:
+- get Enamad
 
+
+seo todo:
 - sitemap
+- search console
 
+seo rules:
 - title for each page
 Example:
 Geotechnical Investigation Services — Your Company Name
 
 - meta desc for each page
-
 - h series for each page
